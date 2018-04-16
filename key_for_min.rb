6f2e75059821a5769b1c4e18299436b40a37cc17
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-04-16 01:06:21 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,6 @@ ikea = {:chair => 25, :table => 85, :mattress => 450}
 def key_for_min_value(name_hash)
   new = name_hash.values
   largest = new[0]
-if(name_hash.size == 0)
-  return nil 
-else
   name_hash.collect do |name, amount| 
     if(amount < largest)
       largest = amount
