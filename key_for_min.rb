4f4084ae9b924efa06b09206839dce4660d8565d
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-04-16 01:07:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,12 @@
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 def key_for_min_value(name_hash)
   new = name_hash.values
-  largest = new[0]
+  smallest = new[0]
   name_hash.collect do |name, amount| 
-    if(amount < largest)
-      largest = amount
+    if(amount < smallest)
+      smallest = amount
     end
-    largest
+    smallest
   end
-  largest
+  smallest
 end
