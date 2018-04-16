79cd12d517c5bba252d83996ec977d8d599d8ff1
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-04-16 01:25:24 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,7 +11,7 @@ def key_for_min_value(name_hash)
     if(amount < smallest)
       smallest = amount
     end
-    "#{smallest} is #{name}"
+    smallest
   end
-  smallest
+  "#{smallest} is #{name}"
 end
